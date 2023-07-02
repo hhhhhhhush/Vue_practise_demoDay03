db591656f8b4cb284e04c1dce5f75348a8f8d358
mzz <2831376148@qq.com>
2023-07-02 15:01:22 +0800

    life

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     我是根组件
+    <my-life></my-life>
   </div>
 </template>
 
 <script>
+import MyLife from '@/components/MyLife.vue'
 export default {
+  components: { MyLife },
 
 }
 </script>
